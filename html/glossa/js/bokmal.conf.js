58f3c451655d2b9df8257965db3caf7013b1fa51
larsnyg <larsnyg@ba8dc031-a62e-0410-8348-de826c2c4041>
2007-05-23 15:11:52 +0000

    added more shortcuts
    git-svn-id: http://omilia.uio.no/svn/repos/glossa@10 ba8dc031-a62e-0410-8348-de826c2c4041

--- html/glossa/js/bokmal.conf.js
+++ html/glossa/js/bokmal.conf.js
@@ -11,7 +11,41 @@ var language='en';
 
 // ** shortcuts ** //
 
-shortcut("Ctrl+Shift+X",function() {
+shortcut("Ctrl+Shift+G",function() {
     addOpt('w','lemma','grunnform');
 });
+
+shortcut("Ctrl+Shift+L",function() {
+    addOpt('w','end','slutten av ordet')
+});
+
+shortcut("Ctrl+Shift+T",function() {
+    addOpt('w','start','starten av ordet')
+});
+
+shortcut("Ctrl+Shift+B",function() {
+    addOpt('w','case','skill store/små bokst.')
+});
+
+// 
+// adj, adv, det, inf, int, konj, prep, pron, subj, subst, tegn, verb, ukjent
+
+shortcut("Ctrl+Shift+A",function() {
+    addOpt('ordkl','adj','adjektive')
+});
+
+shortcut("Ctrl+Shift+D",function() {
+    addOpt('ordkl','adv','adverb')
+});
+
+shortcut("Ctrl+Shift+S",function() {
+    addOpt('ordkl','s','substantiv')
+});
+
+shortcut("Ctrl+Shift+V",function() {
+    addOpt('ordkl','v','verb')
+});
+
+
+
 
