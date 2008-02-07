59698a5762ce779165b45245bdc369192efeed81
larsnyg <larsnyg@ba8dc031-a62e-0410-8348-de826c2c4041>
2008-02-07 19:36:51 +0000

    git-svn-id: http://omilia.uio.no/svn/repos/glossa@99 ba8dc031-a62e-0410-8348-de826c2c4041

--- html/glossa/html/index_dev.php
+++ html/glossa/html/index_dev.php
@@ -1,5 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"
-            "http://www.w3.org/TR/REC-html40/strict.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"  "http://www.w3.org/TR/REC-html40/strict.dtd">
 <html>
  <head>
 
@@ -12,16 +11,21 @@
   $htmlRoot = 'http://omilia.uio.no/glossa/';
   $cgiRoot = 'http://omilia.uio.no/cgi-bin/glossa/';
   $corpus = $_GET['corpus'];
+  $uilang = $_GET['uilang'];
   $def_base_corpus = strtoupper($corpus);
   $subcorpus = $_GET['subcorpus'];
   include("glossa.inc");
+
+if(!$uilang){
+    $uilang = 'no';
+}
+
 ?>
 
 
-
- <link rel="stylesheet" type="text/css" href="
-
-<?php echo $htmlRoot ?>/html/CE.css">
+  
+ <link rel="shortcut icon" href="http://omilia.uio.no/img/favicon.ico" type="image/ico" />
+ <link rel="stylesheet" type="text/css" href="<?php echo $htmlRoot ?>/html/CE.css">
 
 <?php printJsHead();   // ** Glossa ** //   ?>
 
@@ -30,8 +34,10 @@
   if (conf['charset']) {
     charset=conf['charset'];
   }
-  var a = "<meta http-equiv='Content-Type' content='text/html;charset=" + charset + "' >";
+
+var a = "<meta http-equiv='Content-Type' content='text/html;charset=" + charset + "' >";
   document.write(a);
+
 </script>
 
 <?php 
@@ -68,8 +74,16 @@ elseif ($_GET['corpus'] == 'nota' ) {
   echo "<script language='javascript' src='" . $htmlRoot . "/js/nota.js'></script>";
 
 }
-elseif ($_GET['corpus'] == 'upus') {
+elseif ($_GET['corpus'] == 'upus' ) {
   echo "<script language='javascript' src='" . $htmlRoot . "/js/upus.js'></script>";
+
+}
+elseif ($_GET['corpus'] == 'scandiasyn' ) {
+  echo "<script language='javascript' src='" . $htmlRoot . "/js/scandiasyn.js'></script>";
+
+}
+elseif ($_GET['corpus'] == 'taus') {
+  echo "<script language='javascript' src='" . $htmlRoot . "/js/taus.js'></script>";
 
 }
 elseif ( $_GET['corpus'] == 'bokmal' ) {
@@ -83,6 +97,15 @@ elseif ( $_GET['corpus'] == 'samno' ) {
 }
 elseif ( $_GET['corpus'] == 'bul' ) {
   echo "<script language='javascript' src='" . $htmlRoot . "/js/bul.js'></script>";
+}
+elseif ( $_GET['corpus'] == 'euro_news_fr1' ) {
+  echo "<script language='javascript' src='" . $htmlRoot . "/js/euro_news_fr1.js'></script>";
+}
+elseif ( $_GET['corpus'] == 'euro_news_fr2' ) {
+  echo "<script language='javascript' src='" . $htmlRoot . "/js/euro_news_fr2.js'></script>";
+}
+elseif ( $_GET['corpus'] == 'usenet' ) {
+  echo "<script language='javascript' src='" . $htmlRoot . "/js/usenet.js'></script>";
 }
 
 
@@ -108,28 +131,51 @@ elseif ( $_GET['corpus'] == 'bul' ) {
 
 </head>
 <body>
-
 <div style="background-color:gray;color:white">
-<table width='100%'>
+<table width='100%' border='0'>
 <tr>
  <td><b>
-<b> <script language="javascript">document.write(conf['corpus_name'])</script></b>
-
-</b> </td>
- <td>
- Glossa (  
- <?php echo "<a href='" . $cgiRoot . "/get_hits.cgi?corpus=" . $_GET['corpus'] . "'>stored results</a> | "; ?>
- <?php echo "<a href='" . $htmlRoot . "/html/stats.php?corpus=" . $corpus . "&base_corpus=" . $def_base_corpus . "'>statistics</a> | "; ?>
- <?php echo "<a href='" . $htmlRoot . "/html/index_dev.php?fullQuery=yes&corpus=" . $_GET['corpus'] . "'>fullquery</a> | "; ?>
- <?php echo "<a target='_new' href='" . $htmlRoot . "/html/GLOSSA_manual.html'>help</a>"; ?> 
- )
+ <b>
+ <script language="javascript">document.write(conf['corpus_name'])</script></b>
+ </b>
  </td>
-</tr>
+ <td width='150px'>
+
+ </td>
+ <td>
+ Glossa (
+ <?php echo "<a id='stored_results' href='" . $cgiRoot . "/get_hits.cgi?corpus=" . $_GET['corpus'] . "'>stored results</a> | "; ?>
+ <?php
+ if($corpus=='nota' || 'upus' || 'scandiasyn'){
+     echo "<span id='statistics'>statistics</span> | ";
+     echo "<span id='full_query'>full query</span> | ";
+ }
+ else{?>
+ 
+ <?php echo "<a id='statistics' href='" . $htmlRoot . "/html/stats.php?corpus=" . $corpus . "&base_corpus=" . $def_base_corpus . "'>statistics</a> | "; ?>
+ <?php echo "<a id='full_query' href='" . $htmlRoot . "/html/index_dev.php?fullQuery=yes&corpus=" . $_GET['corpus'] . "'>full query</a> | "; ?>
+
+ <?php
+ }
+ ?>
+ <?php echo "<a id='help' target='_new' href='" . $htmlRoot . "/html/GLOSSA_manual.html'>help</a>"; ?> )
+ </td>
+ <td>
+ <?php
+ if($corpus == 'nota'){
+     ?>
+     <span style='color: yellow;cursor: pointer;' id='lang' onclick='toggleLang();'>Norsk</span>
+     <?php
+ }
+ ?>
+</td>
+ <td width='40%'>
+
+ </td>
+ </tr>
 </table>
 
 </div>
-
-
 
  <table>
 <tr>
@@ -158,13 +204,13 @@ elseif ( $_GET['corpus'] == 'bul' ) {
      <td style="background-color:#efefef;border-width:1px;border-style:solid;border-color:#afaeae">
      <script language="javascript">
 
-       document.write("<input type='submit' value='");
+       document.write("<input id='search' type='submit' value='");
      //document.write("<input type='button' onClick='submitForm()' value='");
        document.write(strings[language]['search']);
        document.write("'>");
     </script><br><br>
      <script language="javascript">
-       document.write("<input type='button' onClick='window.location.reload()()' value='");
+       document.write("<input id='reset' type='button' onClick='window.location.reload()()' value='");
        document.write(strings[language]['reset']);
        document.write("'>");
     </script>
@@ -201,8 +247,14 @@ elseif ( $_GET['corpus'] == 'sami' ) {
 elseif ( $_GET['corpus'] == 'nota' ) {
   include("nota.inc");
 }
+elseif ( $_GET['corpus'] == 'scandiasyn' ) {
+  include("scandiasyn.inc");
+}
 elseif ( $_GET['corpus'] == 'upus' ) {
   include("upus.inc");
+}
+elseif ( $_GET['taus'] == 'taus' ) {
+  include("taus.inc");
 }
 elseif ( $_GET['corpus'] == 'bokmal' ) {
   include("bokmal.inc");
@@ -213,6 +265,15 @@ elseif ( $_GET['corpus'] == 'samno' ) {
 
 elseif ( $_GET['corpus'] == 'bul' ) {
   include("bul.inc");
+}
+elseif ( $_GET['corpus'] == 'euro_news_fr1' ) {
+  include("euro_news_fr1.inc");
+}
+elseif ( $_GET['corpus'] == 'euro_news_fr2' ) {
+  include("euro_news_fr2.inc");
+}
+elseif ( $_GET['corpus'] == 'usenet' ) {
+  include("usenet.inc");
 }
 
 ?>
@@ -226,20 +287,31 @@ elseif ( $_GET['corpus'] == 'bul' ) {
  <!-- felles --> 
 <td valign='top' style="background-color:#efefef;border-width:1px;border-style:solid;border-color:#afaeae">
 <?php 
+
  $lang['en']['show_texts']='Show texts'; 
  $lang['en']['save_subcorpus']='Save subcorpus'; 
  $lang['en']['choose_subcorpus']='Choose subcorpus'; 
+ $lang['no']['show_texts']='Vis tekster'; 
+ $lang['no']['save_subcorpus']='Lagre subkorpus'; 
+ $lang['no']['choose_subcorpus']='Velg subkorpus'; 
+
 ?>
-<input type="button" onClick="setAction('<?php echo $cgiRoot ?>/meta_direct.cgi');" value="<?php echo $lang['en']['show_texts'] ?>" /><br><br>
-<input type="button" onClick="setAction('<?php echo $cgiRoot ?>/meta_save_choose.cgi');" value="<?php echo $lang['en']['save_subcorpus'] ?>" />
+<input type="button" id="show_texts" onClick="setAction('<?php echo $cgiRoot ?>/meta_direct.cgi');" value="<?php echo $lang['no']['show_texts'] ?>" /><br><br>
+<?php
+if($corpus != 'nota'){
+?>
+<input type="button" id="save_subcorpus" onClick="setAction('<?php echo $cgiRoot ?>/meta_save_choose.cgi');" value="<?php echo $lang['no']['save_subcorpus'] ?>" />
+<?php
+}
+?>
 <br><br>
 
-<?php 
-
-echo "<a href='" . $cgiRoot . "subcorpus_choose.cgi?corpus=" . $corpus . "'>";
-echo $lang['en']['choose_subcorpus'];
-echo "</a>";
-
+<?php
+if($corpus != 'nota'){
+    echo "<a id='choose_subcorpus' href='" . $cgiRoot . "subcorpus_choose.cgi?corpus=" . $corpus . "'>";
+    echo $lang['no']['choose_subcorpus'];
+    echo "</a>";
+}
 ?>
 
 
@@ -251,7 +323,6 @@ Display:
 <select name='structDisplay'>
 <option></option>
 <script language='javascript'>printDisplayOptions()</script>
-
 </select>
 
 
@@ -277,7 +348,11 @@ if (domLib_isKonq) { document.write("The Glossa interface has significant proble
 </script>
 
 
-<p>Caution: This page is currently undergoing development. <br>Some things will be broken.</td></tr>
+<p><img src="http://omilia.uio.no/img/tri.png" alt="caution"><br />
+Denne Glossaversjonen er under utvikling.<br />
+This version of Glossa is undergoing development.
+</td>
+</tr>
 
 
 </table>
@@ -311,11 +386,17 @@ elseif ( $_GET['corpus'] == 'samno' ) {
 elseif ( $_GET['corpus'] == 'upus' ) {
   include("upus_cred.inc");
 }
+elseif ( $_GET['corpus'] == 'taus' ) {
+  include("taus_cred.inc");
+}
 
 elseif ( $_GET['corpus'] == 'snakkis' ) {
   include("snakkis_cred.inc");
 }
 
+elseif ( $_GET['corpus'] == 'nota' ) {
+  include("nota_cred.inc");
+}
 ?>
 
 </td>
@@ -324,6 +405,14 @@ elseif ( $_GET['corpus'] == 'snakkis' ) {
 
 <script language="javascript">
 setFocus();
+<?php
+ if($corpus == 'nota'){
+     ?>
+ toggleLang();
+     <?php
+ }
+ ?>
+
 </script>
 
 
